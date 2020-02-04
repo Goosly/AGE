11c627c207304e17207f2d17452910edb47f2492
Manu <manu@hivecpq.com>
2020-02-04 16:45:45 +0100

    version++

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
   wcaAppId : 'd9db39d5fd0195bcdc212ead360c748592cd1fd1409b040dd790281da91ef10d',
   wcaSecret : '305623ae24dd7c0789e811e7ae8b364f19e9109c976c0e5ba1c8add1a1773159',
 
-  version: '1.0.012',
+  version: '1.0.014',
   testMode: false
 };
