262112b829edb824446eaac966f3ca028115473d
Manu <manu@hivecpq.com>
2021-10-05 17:09:42 +0200

    Get test running

--- src/common/group.spec.ts
+++ src/common/group.spec.ts
@@ -1,5 +1,3 @@
-// import * as moment from 'moment';
-
 import {GroupService} from './group';
 
 var assert = require('assert');
@@ -11,6 +9,9 @@ describe('decrement', function() {
     };
     let group: GroupService = new GroupService();
 
+    assert.equal(group.decrement(3, event), 2);
+    assert.equal(group.decrement(2, event), 1);
+    assert.equal(group.decrement(1, event), 0);
     assert.equal(group.decrement(0, event), 3);
   });
 });
