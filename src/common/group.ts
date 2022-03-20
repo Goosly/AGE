fd791a230d04f08ed0280c914bdffcd23c643570
Manu Vereecken <manu.vereecken@hotmail.com>
2022-03-20 18:27:16 +0100

    Tslint: const instead of let

--- src/common/group.ts
+++ src/common/group.ts
@@ -33,35 +33,35 @@ export class GroupService {
   }
 
   generateAdvancedGrouping(eventId: EventId, file: Blob) {
-    let generateGroupingForEvent = (e, s) => this.generateGroupingForEvent(e, s);
+    const generateGroupingForEvent = (e, s) => this.generateGroupingForEvent(e, s);
     if (file) {
-      let reader = new FileReader();
+      const reader = new FileReader();
       reader.readAsText(file);
       reader.onload = function(e) {
-        let staff: StaffPerson[] = this.getStaff(e.target['result']);
+        const staff: StaffPerson[] = this.getStaff(e.target['result']);
         generateGroupingForEvent(eventId, staff);
       }.bind(this);
     } else if (this.configuration.autoPickScramblersAndRunners) {
       generateGroupingForEvent(eventId, Helpers.generateStaffBasedOnPersonalBests(this.wcif));
     } else {
-      throw new Error("There are no scramblers and runners! Please select a CSV file or let AGE pick scramblers and runners");
+      throw new Error('There are no scramblers and runners! Please select a CSV file or let AGE pick scramblers and runners');
     }
   }
 
   public getStaff(csvString): StaffPerson[] {
-    let lines: string[] = csvString.includes('\r\n') ? csvString.split('\r\n') : csvString.split('\n');
-    let separator = this.determineSeparator(csvString);
-    let headers = lines[0].split(separator);
+    const lines: string[] = csvString.includes('\r\n') ? csvString.split('\r\n') : csvString.split('\n');
+    const separator = this.determineSeparator(csvString);
+    const headers = lines[0].split(separator);
     this.validateHeaders(headers);
-    let persons = lines.splice(1);
+    const persons = lines.splice(1);
 
-    let staff: StaffPerson[] = [];
+    const staff: StaffPerson[] = [];
     for (let i = 0; i < persons.length; i++) {
-      if (persons[i] === null || persons[i] === undefined || persons[i] === "" || !persons[i].includes(separator)) {
+      if (persons[i] === null || persons[i] === undefined || persons[i] === '' || !persons[i].includes(separator)) {
         continue;
       }
-      let personToAdd = persons[i].split(separator);
-      let staffPerson: StaffPerson = new StaffPerson();
+      const personToAdd = persons[i].split(separator);
+      const staffPerson: StaffPerson = new StaffPerson();
       staffPerson.name = personToAdd[headers.indexOf('name')];
       staffPerson.wcaId = personToAdd[headers.indexOf('wcaId')];
       staffPerson.isAllowedTo = [];
@@ -76,10 +76,10 @@ export class GroupService {
   }
 
   private validateHeaders(headers: string[]) {
-    let expectedHeaders: string[] = ['name', 'wcaId', 'run', '222', '333', '444', '555', '666', '777', '333bf', '333oh', 'clock', 'minx', 'pyram', 'skewb', 'sq1', '444bf', '555bf', '333mbf'];
+    const expectedHeaders: string[] = ['name', 'wcaId', 'run', '222', '333', '444', '555', '666', '777', '333bf', '333oh', 'clock', 'minx', 'pyram', 'skewb', 'sq1', '444bf', '555bf', '333mbf'];
     expectedHeaders.forEach(expectedHeader => {
       if (!headers.includes(expectedHeader)) {
-        let error: string = 'Expected header ' + expectedHeader + ' in the selected csv file, but it was not present.';
+        const error: string = 'Expected header ' + expectedHeader + ' in the selected csv file, but it was not present.';
         alert(error);
         throw new Error(error);
       }
@@ -87,11 +87,11 @@ export class GroupService {
   }
 
   private generateBasicGrouping(eventId: EventId) { // Very simple: random groups
-    let event: any = Helpers.getEvent(eventId, this.wcif);
-    let eventConfiguration: EventConfiguration = event.configuration;
+    const event: any = Helpers.getEvent(eventId, this.wcif);
+    const eventConfiguration: EventConfiguration = event.configuration;
     let i = 0;
     this.shuffleCompetitors();
-    let competitors = this.wcif.persons.filter(p => p[eventId].competing);
+    const competitors = this.wcif.persons.filter(p => p[eventId].competing);
     this.moveTopCompetitorsToPositionsForLastGroup(competitors, event);
     competitors.forEach(p => {
       p[eventId].group = (i + 1) + '';
@@ -102,9 +102,9 @@ export class GroupService {
   }
 
   private generateBasicBySpeedGrouping(eventId: EventId, reverse: boolean) { // Sort by speed, then loop and assign group 1 to first chunk, then group 2, etc.
-    let event: any = Helpers.getEvent(eventId, this.wcif);
-    let eventConfiguration: EventConfiguration = event.configuration;
-    let sizeOfGroup = this.wcif.persons.filter(p => p[eventId].competing).length / eventConfiguration.scrambleGroups;
+    const event: any = Helpers.getEvent(eventId, this.wcif);
+    const eventConfiguration: EventConfiguration = event.configuration;
+    const sizeOfGroup = this.wcif.persons.filter(p => p[eventId].competing).length / eventConfiguration.scrambleGroups;
     Helpers.sortCompetitorsBySpeedInEvent(this.wcif, eventId, reverse);
     this.wcif.persons.filter(p => p[eventId].competing).forEach((p, index) => {
       p[eventId].group = (Math.trunc(index / sizeOfGroup) + 1) + '';
@@ -115,8 +115,8 @@ export class GroupService {
 
   private generateGroupingForEvent(eventId: EventId, staff: StaffPerson[]) {
     // Make some variables
-    let event: any = Helpers.getEvent(eventId, this.wcif);
-    let taskCounter = this.createTaskCounter(event.configuration); // Variable to keep track of assignments for all groups
+    const event: any = Helpers.getEvent(eventId, this.wcif);
+    const taskCounter = this.createTaskCounter(event.configuration); // Variable to keep track of assignments for all groups
 
     this.markPersonsThatArePartOfTheStaff(staff);
 
@@ -133,8 +133,8 @@ export class GroupService {
       alert('Not enough runners for ' + eventId + '!\nMake sure you add plenty of reliable people in your CSV file');
     }
 
-    let group: number = 0; // Group starts counting at 0, so always display as group+1
-    let assignedIds: Array<number> = [];
+    let group = 0; // Group starts counting at 0, so always display as group+1
+    const assignedIds: Array<number> = [];
 
     // 1. Find scramblers, divide them into groups
     potentialScramblers = Helpers.sortScramblersByScramblingAssigned(this.wcif, potentialScramblers);
@@ -193,14 +193,14 @@ export class GroupService {
   private moveTopCompetitorsToPositionsForLastGroup(competitors: Array<any>, event: any) {
     // Not entirely sure anymore what the logic is in this method :|
     if (this.isScrambleDependentEvent(event)) {
-      let topCompetitors = Helpers.getTopCompetitorsBySpeedInEvent(this.wcif, event.id)
+      const topCompetitors = Helpers.getTopCompetitorsBySpeedInEvent(this.wcif, event.id)
         .filter(p => competitors.includes(p));
       if (topCompetitors.length <= competitors.length / event.configuration.scrambleGroups) {
         for (let i = 0; i < topCompetitors.length; i++) {
-          let m = i % event.configuration.stages;
-          let g = Math.trunc(i / event.configuration.stages);
-          let position = (g+1) * this.numberOfGroups(event) - m - 1;
-          let positionOfTopCompetitor = competitors.indexOf(topCompetitors[i]);
+          const m = i % event.configuration.stages;
+          const g = Math.trunc(i / event.configuration.stages);
+          let position = (g + 1) * this.numberOfGroups(event) - m - 1;
+          const positionOfTopCompetitor = competitors.indexOf(topCompetitors[i]);
           if (position === -1 || positionOfTopCompetitor === -1 || positionOfTopCompetitor >= competitors.length) {
             throw new Error('Unexpected error!');
           }
@@ -224,20 +224,20 @@ export class GroupService {
   }
 
   private fillAllUsedTimersWithJudges(eventId: string) {
-    let event: any = Helpers.getEvent(eventId, this.wcif);
+    const event: any = Helpers.getEvent(eventId, this.wcif);
     if (event.configuration.scrambleGroups <= 2) {
       return;
     }
 
-    let numberOfGroups = event.configuration.scrambleGroups * event.configuration.stages;
-    let group: number = 1;
+    const numberOfGroups = event.configuration.scrambleGroups * event.configuration.stages;
+    let group = 1;
     while (group <= numberOfGroups) {
-      let competitors: number = Helpers.countCompetitors(this.wcif, eventId, group);
+      const competitors: number = Helpers.countCompetitors(this.wcif, eventId, group);
       let judges: number = Helpers.countJudges(this.wcif, eventId, group);
 
       if (judges < competitors) {
-        let potentialJudges = this.availableForAnExtraJudgingTask(event, group);
-        let neededJudges: number = competitors - judges;
+        const potentialJudges = this.availableForAnExtraJudgingTask(event, group);
+        const neededJudges: number = competitors - judges;
 
         for (let i = 0; i < neededJudges && i < potentialJudges.length; i++) {
           Helpers.assignExtraJudge(potentialJudges[i], eventId, group);
@@ -249,15 +249,15 @@ export class GroupService {
   }
 
   private availableForAnExtraJudgingTask(event: any, group: number): any[] {
-    let potentialJudges = this.wcif.persons.filter(p => p[event.id].competing
+    const potentialJudges = this.wcif.persons.filter(p => p[event.id].competing
       && this.canJudge(p)
       && Helpers.notAssignedToAnythingYetInGroup(p[event.id].group, event, group));
     return Helpers.sortByCompetingToTaskRatio(this.wcif, event.id, potentialJudges);
   }
 
   private swapAssignments(a: Person, b: Person, event: any) {
-    let assignmentA = a[event.id].group;
-    let assignmentB = b[event.id].group;
+    const assignmentA = a[event.id].group;
+    const assignmentB = b[event.id].group;
     a[event.id].group = assignmentB;
     b[event.id].group = assignmentA;
   }
@@ -267,7 +267,7 @@ export class GroupService {
   }
 
   private markPersonsThatArePartOfTheStaff(staff: StaffPerson[]) {
-    let staffWcaIds = staff.map(s => s.wcaId);
+    const staffWcaIds = staff.map(s => s.wcaId);
     this.wcif.persons.forEach(p => p.isStaff = staffWcaIds.includes(p.wcaId));
   }
 
@@ -280,7 +280,7 @@ export class GroupService {
   }
 
   decrement(group: number, event: any): number {
-    let numberOfGroups = this.numberOfGroups(event);
+    const numberOfGroups = this.numberOfGroups(event);
     return (group + numberOfGroups - 1) % numberOfGroups;
   }
 
@@ -301,7 +301,7 @@ export class GroupService {
       throw new Error('No competitors');
     }
 
-    for (let e of this.wcif.events) {
+    for (const e of this.wcif.events) {
       e.numberOfRegistrations = 0; // Add field
       if (! e.rounds || ! e.rounds.length) {
         alert('No rounds found for ' + e.id + '! Please define all rounds and the schedule on the WCA website and then restart.');
@@ -313,7 +313,8 @@ export class GroupService {
 
       this.determineStartTimeOfEvent(e);
     }
-	  // All events should have a startTime now (if they're included in the schedule)
+
+    // All events should have a startTime now (if they're included in the schedule)
     this.sortEventsByStartTime();
 
     this.processRegistrationsOfPersons();
@@ -323,8 +324,8 @@ export class GroupService {
 
   private processRegistrationsOfPersons() {
     // For every person: set registration fields per event to 1 or 0 (and count per event)
-    let idsToRemove = [];
-    for (let p of this.wcif.persons) {
+    const idsToRemove = [];
+    for (const p of this.wcif.persons) {
       p.fullName = p.name;
       p.name = p.name.split('(')[0]; // Remove local name
 
@@ -332,7 +333,7 @@ export class GroupService {
         idsToRemove.push(p.registrantId);
         continue;
       }
-      for (let e of this.wcif.events) {
+      for (const e of this.wcif.events) {
         if (p.registration.eventIds.indexOf(e.id) > -1) {
           p[e.id] = {competing: true, group: '1'};
           e.numberOfRegistrations++;
@@ -363,14 +364,14 @@ export class GroupService {
   public importAssignmentsFromWcif(): void {
     this.resetGroupsForAllCompetitors();
 
-    let allActivities: Activity[] = ActivityHelper.getAllActivitiesFromWcif(this.wcif);
+    const allActivities: Activity[] = ActivityHelper.getAllActivitiesFromWcif(this.wcif);
     this.wcif.persons.forEach(p => this.readPersonAssignmentsFromWcif(p, allActivities));
     this.calculateStagesOfAllEvents();
   }
 
   private calculateStagesOfAllEvents() {
     this.wcif.events.forEach(event => {
-      let groups = Helpers.countGroupsForEvent(this.wcif, event);
+      const groups = Helpers.countGroupsForEvent(this.wcif, event);
       if (groups > 0) {
         event.configuration.stages = groups / event.configuration.scrambleGroups;
       } else {
@@ -383,18 +384,18 @@ export class GroupService {
     this.wcif.persons.forEach(p => {
       this.wcif.events.forEach(e => {
         p[e.id].group = '';
-      })
+      });
     });
   }
 
   private readPersonAssignmentsFromWcif(p: Person, allActivities: Activity[]) {
     this.sortAssignmentsByAssignmentCode(p);
     p.assignments.forEach(assignmentFromWcif => {
-      let activity = allActivities.filter(a => a.id.toString() === assignmentFromWcif.activityId.toString());
+      const activity = allActivities.filter(a => a.id.toString() === assignmentFromWcif.activityId.toString());
       if (activity.length === 0) {
         return;
       }
-      let code = parseActivityCode(activity[0].activityCode);
+      const code = parseActivityCode(activity[0].activityCode);
       if (!! code.attemptNumber && code.attemptNumber !== 1) {
         return;
       }
@@ -430,23 +431,23 @@ export class GroupService {
   }
 
   importAssignmentsFromCsv(callback: (competitors: number) => void) {
-    let file = document.getElementById('importCsv')['files'][0];
+    const file = document.getElementById('importCsv')['files'][0];
     if (file) {
-      let reader = new FileReader();
+      const reader = new FileReader();
       reader.readAsText(file);
       reader.onload = function(e) {
-        let csv: string = e.target['result'];
-        let lines: string[] = csv.includes('\r\n') ? csv.split('\r\n') : csv.split('\n');
-        let separator = this.determineSeparator(csv);
-        let headers = lines[0].split(separator);
-        let competitors = lines.splice(1);
+        const csv: string = e.target['result'];
+        const lines: string[] = csv.includes('\r\n') ? csv.split('\r\n') : csv.split('\n');
+        const separator = this.determineSeparator(csv);
+        const headers = lines[0].split(separator);
+        const competitors = lines.splice(1);
         let importedCompetitorsCounter = 0;
         for (let i = 0; i < competitors.length; i++) {
-          if (competitors[i] === null || competitors[i] === undefined || competitors[i] === "" || !competitors[i].includes(separator)) {
+          if (competitors[i] === null || competitors[i] === undefined || competitors[i] === '' || !competitors[i].includes(separator)) {
             continue;
           }
-          let competitorToImport = competitors[i].split(separator);
-          let matchingPersons = this.wcif.persons.filter(p => p.name === competitorToImport[0] || p.fullName === competitorToImport[0]);
+          const competitorToImport = competitors[i].split(separator);
+          const matchingPersons = this.wcif.persons.filter(p => p.name === competitorToImport[0] || p.fullName === competitorToImport[0]);
           if (matchingPersons.length === 0) {
             continue;
           }
@@ -459,8 +460,8 @@ export class GroupService {
         callback(importedCompetitorsCounter);
       }.bind(this);
     } else {
-      alert("Please select a CSV file to import first");
-      throw Error("No CSV file to import");
+      alert('Please select a CSV file to import first');
+      throw Error('No CSV file to import');
     }
   }
 
@@ -475,7 +476,7 @@ export class GroupService {
   }
 
   public setDefaultEventConfiguration() {
-    let defaults : Array<EventConfiguration> = [
+    const defaults: Array<EventConfiguration> = [
       { id: '222', stages: 1, scramblers: 2, runners: 2, timers: this.configuration.totalNumberOfTimers, totalTimers: this.configuration.totalNumberOfTimers, skip: false, scrambleGroups: 2 },
       { id: '333', stages: 1, scramblers: 2, runners: 2, timers: this.configuration.totalNumberOfTimers, totalTimers: this.configuration.totalNumberOfTimers, skip: false, scrambleGroups: 2 },
       { id: '444', stages: 1, scramblers: 2, runners: 2, timers: this.configuration.totalNumberOfTimers, totalTimers: this.configuration.totalNumberOfTimers, skip: false, scrambleGroups: 2 },
@@ -496,7 +497,7 @@ export class GroupService {
       { id: '333fm', stages: 1, scramblers: 2, runners: 0, timers: this.configuration.totalNumberOfTimers, totalTimers: this.configuration.totalNumberOfTimers, skip: true, scrambleGroups: 1 },
     ];
 
-    for (let e of this.wcif.events) {
+    for (const e of this.wcif.events) {
       e.configuration = defaults.filter(d => d.id === e.id)[0];
       e.configuration.scrambleGroups = Math.max(e.round1.scrambleSetCount, e.configuration.scrambleGroups);
     }
@@ -516,7 +517,7 @@ export class GroupService {
   }
 
   private canScramble(person, staff: StaffPerson[], event): boolean {
-    let x = staff.filter(s => s.wcaId === person.wcaId);
+    const x = staff.filter(s => s.wcaId === person.wcaId);
     if (x.length === 1) {
       return x[0].isAllowedTo.indexOf('scrambleEverything') > -1 || x[0].isAllowedTo.indexOf(event) > -1;
     }
@@ -524,7 +525,7 @@ export class GroupService {
   }
 
   private canRun(person, staff: StaffPerson[]): boolean {
-    let x = staff.filter(s => s.wcaId === person.wcaId);
+    const x = staff.filter(s => s.wcaId === person.wcaId);
     if (x.length === 1) {
       return x[0].isAllowedTo.indexOf('run') > -1;
     }
@@ -532,7 +533,7 @@ export class GroupService {
   }
 
   private shuffleCompetitors() {
-    let i,j,x;
+    let i, j, x;
     for (i = this.wcif.persons.length - 1; i > 0; i--) {
       j = Math.floor(Math.random() * (i + 1));
       x = this.wcif.persons[i];
@@ -543,8 +544,8 @@ export class GroupService {
 
   private sortEventsByStartTime() {
     this.wcif.events = this.wcif.events.sort(function(a, b) {
-      var textA = a.startTime;
-      var textB = b.startTime;
+      const textA = a.startTime;
+      const textB = b.startTime;
       if (textA === '') {
         return 1;
       }
@@ -556,8 +557,8 @@ export class GroupService {
   }
 
   private createTaskCounter(configuration: EventConfiguration) {
-    let numberOfGroups: number = configuration.stages * configuration.scrambleGroups;
-    let tasks = [
+    const numberOfGroups: number = configuration.stages * configuration.scrambleGroups;
+    const tasks = [
       {	'J': { 'max': configuration.timers, 'count': 0 },
         'R': { 'max': configuration.runners, 'count': 0 },
         'S': { 'max': configuration.scramblers, 'count': 0 } }
