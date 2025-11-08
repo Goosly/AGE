a431ed1fe27c41fc3f49eea6dc8a42ad7a525acd
Manu <manu.vereecken@hotmail.com>
2025-11-08 14:12:34 +0100

    build

--- src/app/dialog/confirm-save-wcif-dialog.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'confirm-save-wcif-dialog',
-  templateUrl: 'confirm-save-wcif-dialog.component.html',
-})
-export class ConfirmSaveWcifDialogComponent {
-
-}
